984640d7299b4066b94a594059f9460f49fd5fb8
zeronakysa <aurelien.delagarde@gmail.com>
2018-03-08 09:58:33 +0100

    Add QR Generator

--- Mission1/QRCodeMaker/main.c
+++ Mission1/QRCodeMaker/main.c
@@ -2,3 +2,29 @@
 // Created by zero on 26/02/18.
 //
 
+#include "lib/qrcode.h"
+
+#include <stdio.h>
+
+void main(int argc, char** argv){
+
+    // The structure to manage the QR code
+    QRCode qrcode;
+
+    // Allocate a chunk of memory to store the QR code
+    uint8_t qrcodeBytes[qrcode_getBufferSize()];
+
+    qrcode_initText(&qrcode, qrcodeBytes, 3, ECC_LOW, "HELLO WORLD");
+
+    for (uint8_t y = 0; y < qrcode.size; y++) {
+        for (uint8_t x = 0; x < qrcode.size; x++) {
+            if (qrcode_getModule(&qrcode, x, y){
+                printf("**");
+            } else {
+                printf("  ");
+
+            }
+        }
+        printf("\n");
+    }
+}
