db5c749b56e305be1b2c056fbb2122c29b2288c2
zeronakysa <aurelien.delagarde@gmail.com>
2018-03-08 10:22:13 +0100

    Add QR Generator fix

--- Mission1/QRCodeMaker/main.c
+++ Mission1/QRCodeMaker/main.c
@@ -1,30 +1,28 @@
-//
-// Created by zero on 26/02/18.
-//
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "lib/qrcode.h"
 
-#include <stdio.h>
-
-void main(int argc, char** argv){
+int main() {
 
     // The structure to manage the QR code
     QRCode qrcode;
 
-    // Allocate a chunk of memory to store the QR code
-    uint8_t qrcodeBytes[qrcode_getBufferSize()];
+// Allocate a chunk of memory to store the QR code
+    uint8_t qrcodeBytes[qrcode_getBufferSize((uint8_t)5000)];
 
-    qrcode_initText(&qrcode, qrcodeBytes, 3, ECC_LOW, "HELLO WORLD");
+    qrcode_initText(&qrcode, qrcodeBytes, 3, ECC_LOW, "BONJOUR LE PEUPLE");
 
     for (uint8_t y = 0; y < qrcode.size; y++) {
         for (uint8_t x = 0; x < qrcode.size; x++) {
-            if (qrcode_getModule(&qrcode, x, y){
-                printf("**");
+            if (qrcode_getModule(&qrcode, x, y)) {
+                printf("##");
             } else {
                 printf("  ");
-
             }
         }
         printf("\n");
     }
+
+    printf("BONJOUR LE PEUPLE\n");
+    return 0;
 }
